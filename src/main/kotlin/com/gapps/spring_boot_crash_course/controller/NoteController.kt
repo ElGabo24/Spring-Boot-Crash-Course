802487aab5d24c8bf2c702f8bcce5699c9d9053b
user <gterrazas@tecnologiza.me>
2025-08-16 21:45:42 -0600

    Cambios en el note controller

---
+++ src/main/kotlin/com/gapps/spring_boot_crash_course/controller/NoteController.kt
@@ -0,0 +1,85 @@
+package com.gapps.spring_boot_crash_course.controller
+
+import com.gapps.spring_boot_crash_course.controller.NoteController.NoteResponse
+import com.gapps.spring_boot_crash_course.database.model.Note
+import com.gapps.spring_boot_crash_course.database.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+// POST https://localhost:8080/notes
+// GET https://localhost:8080/notes?ownerId=123
+// DELETE https://localhost:8080/notes/123
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NoteRepository
+) {
+
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant
+    )
+
+    @PostMapping
+    fun save(
+        @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId()
+            )
+        )
+
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map { note ->
+            note.toResponse()
+        }
+    }
+
+    @DeleteMapping(path = ["/{id}"])
+    fun deleteById(@PathVariable id: String) {
+        repository.deleteById(ObjectId(id))
+    }
+
+
+}
+
+private fun Note.toResponse(): NoteResponse {
+    return NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = createdAt
+    )
+}
